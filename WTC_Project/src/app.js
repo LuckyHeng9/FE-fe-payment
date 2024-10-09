8f601cb84b1df000fab0f93fdc2fe79f9cedfe9d
LuckyHeng9 <123loemheng@gmail.com>
2024-10-09 22:16:27 +0700

    New

--- WTC_Project/src/app.js
+++ WTC_Project/src/app.js
@@ -1,3 +1,5 @@
 // Get the button element and the menu icon image
 let btnmenu = document.querySelector('.menu-btn');
-let menuIcon = document.querySelector('#menu-icon');
+let menuIcon = document.querySelector('#menu-icon')
+
+happy = sad-code;
