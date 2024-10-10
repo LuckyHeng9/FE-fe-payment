f121432e1c62328c5ff7ad71ac1548bd41fe235f
LuckyHeng9 <123loemheng@gmail.com>
2024-10-10 18:42:34 +0700

    Udate commit

--- WTC_Project/src/app.js
+++ WTC_Project/src/app.js
@@ -1,5 +1,20 @@
 // Get the button element and the menu icon image
 let btnmenu = document.querySelector('.menu-btn');
-let menuIcon = document.querySelector('#menu-icon')
+let menuIcon = document.querySelector('#menu-icon');
+let sidebar = document.querySelector('.side-bar');
 
-happy = sad-code;
+btnmenu.addEventListener('click', () => {
+    // Toggle 'active' class on nav links
+    sidebar.classList.toggle('active');
+
+    // Check if nav links have the 'active' class
+    if (sidebar.classList.contains('active')) {
+        // Change the menu icon to a close icon
+        menuIcon.classList.remove('fa-bars');
+        menuIcon.classList.add('fa-times');
+    } else {
+        // Revert back to the hamburger icon
+        menuIcon.classList.remove('fa-times');
+        menuIcon.classList.add('fa-bars');
+    }
+});
